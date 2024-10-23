818cd4e3ab9bfff520a8dd2cf6306ead4cd7d2d0
Ken Fukuyama <kenfdev@gmail.com>
2024-10-23 06:42:50 +0000

    add typed sql

---
+++ prisma/sql/searchArticlesByBody.sql
@@ -0,0 +1,1 @@
+SELECT title FROM "Article" WHERE body LIKE likequery($1);
--- src/search.ts
+++ src/search.ts
@@ -1,17 +1,21 @@
 import { PrismaClient } from '@prisma/client';
+import { searchArticlesByBody } from '@prisma/client/sql';
 const prisma = new PrismaClient();
 
 async function searchArticles(keyword: string) {
   // await prisma.$queryRawUnsafe(`SET enable_bitmapscan TO off;`);
 
   console.time('searchArticles');
-  const articles = await prisma.$queryRawUnsafe(
-    `SELECT title FROM "Article" WHERE body LIKE likequery($1)`,
-    keyword
-  );
+  // const articles = await prisma.$queryRawUnsafe<any[]>(
+  //   `SELECT title FROM "Article" WHERE body LIKE likequery($1)`,
+  //   keyword
+  // );
+  const articles = await prisma.$queryRawTyped(searchArticlesByBody(keyword));
   console.timeEnd('searchArticles');
 
+  console.log(`count: ${articles.length}`);
+  console.log(`first title: ${articles[0].title}`);
   // await prisma.$queryRawUnsafe(`SET enable_bitmapscan TO on;`);
 }
 
-searchArticles('テスト');
+searchArticles('日本');
